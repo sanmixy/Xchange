a355efab2dbc7a17f1030617e850104850f44b1f
Xnus <sanmixy@gmail.com>
2016-12-04 13:49:55 +0800

    add datasource model

--- Xchange_master/datasource/models.py
+++ Xchange_master/datasource/models.py
@@ -1,3 +1,26 @@
+from django.contrib.auth.models import User
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+from system.models import Department, System
+
+
+class DataSource(models.Model):
+    source_types = (
+        ('DB', 'database'),
+        ('FTP', 'ftp'),
+        ('SFTP', 'sftp')
+    )
+    create_user = models.ForeignKey(User, help_text='the user who create this datasource')
+    create_time = models.DateTimeField(timezone.now(), help_text='create time')
+    source_type = models.CharField(choices=source_types, max_length=5, help_text='specify the type of this datasource')
+    host = models.GenericIPAddressField(protocol='both', unpack_ipv4=False, help_text='ip address of this datasource')
+    port = models.IntegerField(default=0, help_text='port of this datasource')
+    department = models.ForeignKey(Department)
+    system = models.ForeignKey(System)
+
+    def __str__(self):
+        return self.host
+
+    class Meta:
+        db_table = 'DATASOURCE'
